53f5ea0af59f8ffcbbb104bfffaab89d432438d7
Haluey <150896043+minchochocho@users.noreply.github.com>
2026-02-03 17:25:45 +0900

    02차시_퀴즈수정
    17:25
    02/03 끝

--- Day02/Chap04-Solution/Chap04-quiz2/main.c
+++ Day02/Chap04-Solution/Chap04-quiz2/main.c
@@ -1,9 +1,12 @@
 ﻿#include<stdio.h>
 
 int main() {
-	int seats = 70;
-	int audience = 65;
-	double rate = 0;
+	int seats = 0;	// 자리 초기화
+	int audience =0;	// 관객수 초기화
+	double rate = 0;	//입장률 초기화
+
+	seats = 70;		// 전체 자리수
+	audience = 65;	// 관객 수 
 
 	rate = (double)audience / seats*100;	// 백분율을 구하기 위해 100곱하기
 
