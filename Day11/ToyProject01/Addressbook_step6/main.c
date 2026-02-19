13dc999bd79a81db3ef306ab452df48db43f4db5
minchochocho <150896043+minchochocho@users.noreply.github.com>
2026-02-19 17:28:06 +0900

    10, 11차시 마무리
    토이 프로젝트 - 주소록 프로그램 마무리

---
+++ Day11/ToyProject01/Addressbook_step6/main.c
@@ -0,0 +1,563 @@
+﻿// 주소록 프로그램 step6
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#pragma region 전처리 영역
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+
+#pragma endregion
+
+#pragma region 매크로 선언 영역
+
+// 사람은 숫자보다 이름에 익숙. 유지보수를 위해서
+#define MAX_CONTACTS  100    // 100명 주소록
+#define NAME_LEN	   31	 // 이름 최대길이
+#define PHONE_LEN      21
+#define EMAIL_LEN      65
+#define ADDR_LEN	   121
+#define MEMO_LEN	   31
+
+#pragma endregion
+
+
+typedef struct _contact {
+	char name[NAME_LEN];
+	char phone[PHONE_LEN];
+	char address[ADDR_LEN];
+	char email[EMAIL_LEN];
+	char memo[MEMO_LEN];
+} Contact;   // 이후 Contact 이름으로 사용
+
+#pragma region 변수 영역
+
+// 변수 선언
+static Contact contacts[MAX_CONTACTS];
+static int count = 0;
+
+#pragma endregion 
+
+#pragma region 함수 영역
+
+// 함수 선언
+
+//step1
+static void print_menu();	// static - 이 소스 내에서만 쓰겠다
+static int read_menu();
+static void read_line(char* buf, int size);
+
+//step2
+static void add_contact();
+static void list_contact();
+
+//step3
+static void search_contact();
+static void print_contact(int);
+
+//step4
+static void update_contact();
+static void delete_contact();
+
+//step5
+static int save_contacts(const char* filename);	// 파일명이 변경되면 안됨
+static int load_contacts(const char* filename);
+static void trim_newline(char* s);		// 줄바꿈 제거기능 추가
+
+//step6
+static int contains_pipe(const char* s);		// 입력 문자열중에
+static int find_by_phone(const char* phone);	// 같은 전화번호 중복체크
+static void sort_by_name();		// 이름순 정렬  qsort() 함수 사용, stdlib.h에 포함
+static int cmp_contact_name(const void* a, const char* b);
+
+
+#pragma endregion 
+
+#pragma region 메인함수 영역
+int main() {
+	int choice = 0;
+
+	load_contacts("contacts.txt");	// 프로그램 실행 후 데이터 로드!
+
+	while (1) {	// 무한루프
+		print_menu();	// 메뉴를 출력
+		choice = read_menu();	// choice 값에 입력을 받음
+
+		switch (choice) {
+		case 1:
+			//printf("%s\n", "[ADD] 연락처 추가 (기능 구현요)");
+			add_contact();		// 실제 연락처 추가
+			break;
+
+		case 2:
+			//puts("[LIST] 연락처 목록");
+			list_contact();
+			break;
+
+		case 3:
+			//puts("[SEARCH] 연락처 검색");
+			search_contact();
+			break;
+
+		case 4:
+			//puts("[EDIT] 연락처 수정");
+			update_contact();
+			break;
+
+		case 5:
+			//puts("[DELETE] 연락처 삭제");
+			delete_contact();
+			break;
+
+		case 6:
+			//puts("정렬");
+			sort_by_name();
+			break;
+
+		case 7:
+			save_contacts("contacts.txt");	// 종료 직전 현재 데이터 저장
+			puts("프로그램 종료");
+			return 0;
+
+		default:
+			puts("메뉴는 1~7 사이 입니다.");
+			break;
+		}
+
+		puts("");
+
+	}
+
+	return 0;
+
+}
+#pragma endregion
+#pragma region 사용자함수 정의 영역
+
+// 메뉴 출력
+static void print_menu() {
+	puts("========================================");
+	puts("          주소록 프로그램 (Step 6)      ");
+	puts("========================================");
+	puts("1. 추가");
+	puts("2. 목록");
+	puts("3. 검색");
+	puts("4. 수정");
+	puts("5. 삭제");
+	puts("6. 정렬");
+	puts("7. 종료");
+	// puts("8. 저장");	// 데이터 저장
+	// puts("9. 로드");	// 데이터 로드
+	puts("----------------------------------------");
+
+}
+
+// 문자열에 |가 존재하는지
+static int contains_pipe(const char* s) {
+
+	return(strchr(s, '|') != NULL);	// 발견되 |이후의 문자열이 리턴
+}
+
+// 전화번호 중복 체크함수
+static int find_by_phone(const char* phone) {
+	int i;
+
+	for ( i = 0; i < count; i++) {
+		if (strcmp(contacts[i].phone, phone) == 0)
+			return i;	// 현재 인덱스값의 폰번호가 같다
+	}
+	return -1;	// 일치하는게 없음
+}
+
+// 두 주소록 이름 비교함수
+static int cmp_contact_name(const void* a, const char* b) {
+	const Contact* ca = (const Contact*)a;
+	const Contact* cb = (const Contact*)b;
+
+
+	return strcmp(ca->name, (*cb).name);	// 1, 0, -1
+}
+
+
+// 메뉴 읽기 함수
+static int read_menu() {
+	int choice, ch = 0;
+
+	printf("선택 > ");
+	if (scanf("%d", &choice) != 1) {	// 숫자면 초이스에 저장하고 숫자가 아니면 -1을 반환
+
+		// 버퍼 비우기 : scanf는 숫자만 가져가고 버퍼에 엔터를 남기기에
+		// 찌꺼기를 치우지 않으면 다음에 오는 입력함수에서 엔터를 입력으로 착각해 빈문자열을 읽을 수 잇음
+		while ((ch = getchar()) != '\n' && ch != EOF) { }	
+		// 엔터를 만날때까지 모든 문자를 읽어서 버림
+		return -1;
+	}
+
+	// scanf 뒤에 있는 버퍼에 엔터값 제거
+	while ((ch = getchar()) != '\n' && ch != EOF) { }
+
+	return choice;
+}
+
+
+// fgets로 입력할 때 문제가 발생하지 않도록 문자열 끝에 '\n' 제거
+static void read_line(char* buf, int size) {
+	if ((fgets(buf, size, stdin) == NULL)) {	// size만큼 stdin에서 문자열을 읽어 buf에 저장 => 이게 NULL일때
+		buf[0] = '\n';
+		return;
+	}
+	// strcspn(buf,"\n") = buf 문자열 내에서 \n을 찾아서 그 위치값을 리턴
+	// ex) buf = "Hello World!\n" -> "Hello World!\0"
+	// buf[12]='\0'
+	buf[strcspn(buf, "\n")] = '\0';		// 문자열 끝 \n을 \0으로 변경
+
+}
+
+// 문자열 마지막에 엔터가 있으면 제거하는 함수
+static void trim_newline(char* s) {
+	s[strcspn(s, "\n")] = '\0';
+}
+
+
+// 메뉴 1번 기능. 연락처 추가
+static void add_contact() {
+	if (count >= MAX_CONTACTS) {	// 지정된 크기만큼만
+		puts("주소록 최대인원인 100명에 도달했습니다.");
+		return;
+	}
+
+	puts("[ADD] 새 연락처 입력");
+
+	printf("이름[최대10자] : ");
+	read_line(contacts[count].name, NAME_LEN);	// read_line(시작주소, 최대크기)
+	if (contains_pipe(contacts[count].name)) {	// 이름에 파이프가 들어가면
+		puts(" '|' 문자는 사용할 수 없습니다. 다시 추가하세요.");
+		return;	// 함수 종료
+	}
+
+	printf("전화[최대20자] : ");
+	read_line(contacts[count].phone, PHONE_LEN);
+	if (strlen(contacts[count].phone)==0) {
+		puts("전화번호는 필수입니다.");
+		return;
+	}
+
+	{ // 영역을 구분짓는 코드블럭
+		int dup = find_by_phone(contacts[count].phone);
+		if (dup != -1) {
+			printf("동일 전화번호 존재 (순번 : %3d, 이름 : %10s\n", dup+1, contacts[dup].name);
+			puts("추가 취소");
+			return;
+		}
+
+	}
+	
+	printf("주소[최대40자] : ");
+	read_line(contacts[count].address, ADDR_LEN);
+	if (contains_pipe(contacts[count].address)) {	// 이름에 파이프가 들어가면
+		puts(" '|' 문자는 사용할 수 없습니다. 다시 추가하세요.");
+		return;	// 함수 종료
+	}
+
+	printf("이메일 : ");
+	read_line(contacts[count].email, EMAIL_LEN);
+
+	printf("메모[최대10자] : ");
+	read_line(contacts[count].memo, MEMO_LEN);
+	if (contains_pipe(contacts[count].memo)) {	// 이름에 파이프가 들어가면
+		puts(" '|' 문자는 사용할 수 없습니다. 다시 추가하세요.");
+		return;	// 함수 종료
+	}
+
+	count++;
+	puts("추가 완료!");
+
+}
+
+// 메뉴 2. 주소록 목록
+static void list_contact() {
+	int i;
+
+	if (count == 0) {	// 저장된 데이터가 하나도 없을때
+		puts("[LIST] 저장된 연락처 없음");
+		return;
+	}
+
+	// %-ns << 왼쪽정렬
+	puts("[LIST] 연락처 목록");
+	puts("------------------------------------------------------------------------------------------");
+	printf("%4s | %-10s | %-17s | %-30s | %-25s\n", "순번", "이름", "전화", "주소", "이메일");
+	puts("------------------------------------------------------------------------------------------");
+
+	// contacts 배열이 들어있는 주소록 출력
+	for (i = 0; i < count; i++) {	// 저장된 인원수 만큼 루프 돌리기
+		printf("%4d | %-11s | %-15s | %-30s | %-25s\n",
+			i + 1, contacts[i].name, contacts[i].phone, contacts[i].address, contacts[i].email);
+	}
+	puts("------------------------------------------------------------------------------------------");
+}
+
+// 메뉴3. 검색함수
+static void search_contact() {
+	char keyword[NAME_LEN];
+	int found = 0;
+	int i;
+
+	if (count == 0) {
+		puts("검색할 연락처가 없습니다.");
+		return;
+	}
+
+	printf("검색할 이름 입력 : ");
+	read_line(keyword, NAME_LEN);	// reade_line으로 키워드를 입력받기
+
+	if (strlen(keyword) == 0) {
+		puts("검색할 이름이 없습니다.");
+		return;
+	}
+
+	puts("검색 결과 : ");
+	puts("----------------------------------------");
+
+	for (int i = 0; i < count; i++) {
+		// strcmp와 유사한 기능, strstr(찾을 문자열, 찾는 문자열) 같은 문자열이 있으면 그 문자열 리턴
+		// ex) 길동 > 홍길동, 동 > 홍길동
+		if (strstr(contacts[i].name, keyword) != NULL) {	// 일치하는게 있으면
+			printf("순번 : %d\n", i + 1);
+			print_contact(i);	// 상세 출력
+			puts("----------------------------------------");
+			found = 1;
+		}
+	}
+
+	if (found < 1) {	// 예외처리
+		puts("일치하는 연락처가 없습니다.");
+	}
+}
+
+// 메뉴 3-1. 주소록 1건 상세출력 함수
+static void print_contact(int index) {
+	printf("이름   : %s\n", contacts[index].name);
+	printf("전화   : %s\n", contacts[index].phone);
+	printf("주소   : %s\n", contacts[index].address);
+	printf("이메일 : %s\n", contacts[index].email);
+	printf("메모   : %s\n", contacts[index].memo);
+}
+
+// 메뉴 4. 주소록 수정함수
+static void update_contact() {
+	char buf[MEMO_LEN];
+	int index, ch;
+
+	if (count==0) {
+		puts("수정할 연락처가 없습니다.");
+		return;
+	}
+
+	printf("[UPDATE] 수정할 번호 (1 ~ %d) > ", count);
+	if (scanf("%d",&index)!=1) {	// 입력 실패면
+		while ((ch = getchar()) != '\n' && ch != EOF) { }
+		puts("숫자 입력 요망");
+		return;
+	}
+
+	// 이전 버퍼에 남아있는 데이터 삭제 기능
+	while ((ch = getchar()) != '\n' && ch != EOF) { }
+
+	if (index<1 || index>count) {
+		puts("입력범위 초과");
+		return;
+	}
+
+	puts("현재 정보:");
+	print_contact(index - 1);
+	puts("----------------------------------------");
+	// 일단 검색해서 나온 결과 보기까지 구현
+
+	// 실제 수정 로직
+	puts("새 값 입력 (엔터만 입력하면 기존값 유지)");
+
+	printf("이름 (%s) > ", contacts[index - 1].name);
+	read_line(buf, NAME_LEN);	// 엔터만 치면 buf[0]에 \0을 넣게됨
+	if (buf[0]!='\0') {	// 이 조건을 걸어 값이 입력되었을때만 메모리를 덮어쓰게함
+		// strncpy는 복사할 문자열이 대상 버퍼크기와 같거나 크면 끝에 \0을 붙이지 않는 특징이 있음
+		strncpy(contacts[index - 1].name, buf, NAME_LEN);
+		contacts[index - 1].name[NAME_LEN - 1] = '\0';	// 안정빵용 추가. 글자를 MAX 적으면 \0이 들어가야 문자열의 끝을 알려줌
+
+	}
+	
+	printf("전화 (%s) > ", contacts[index - 1].phone);
+	read_line(buf, PHONE_LEN);
+	if (buf[0] != '\0') {	// 엔터를 친게 아니면, 내용을 입력했으면
+		strncpy(contacts[index - 1].phone, buf, PHONE_LEN);
+		contacts[index - 1].phone[PHONE_LEN - 1] = '\0';
+	}
+
+	printf("주소 (%s) > ", contacts[index - 1].address);
+	read_line(buf, ADDR_LEN);
+	if (buf[0] != '\0') {	// 엔터를 친게 아니면, 내용을 입력했으면
+		strncpy(contacts[index - 1].address, buf, ADDR_LEN);
+		contacts[index - 1].address[ADDR_LEN - 1] = '\0';	
+	}
+
+	printf("이메일 (%s) > ", contacts[index - 1].email);
+	read_line(buf, EMAIL_LEN);
+	if (buf[0] != '\0') {	// 엔터를 친게 아니면, 내용을 입력했으면
+		strncpy(contacts[index - 1].email, buf, EMAIL_LEN);
+		contacts[index - 1].email[EMAIL_LEN - 1] = '\0';
+	}
+
+	printf("메모 (%s) > ", contacts[index - 1].memo);
+	read_line(buf, MEMO_LEN);
+	if (buf[0] != '\0') {	// 엔터를 친게 아니면, 내용을 입력했으면
+		strncpy(contacts[index - 1].memo, buf, MEMO_LEN);
+		contacts[index - 1].memo[MEMO_LEN - 1] = '\0';
+	}
+
+	puts("수정 완료!");
+}
+
+// 메뉴 5. 주소 삭제함수
+static void delete_contact() {
+	int index, i, ch;
+	char yn[8];
+
+	if (count == 0) {
+		puts("삭제할 연락처가 없습니다.");
+		return;
+	}
+
+	printf("[DELETE] 삭제할 번호 (1~%d) > ", count);
+	if (scanf("%d",&index)!=1) {	// 입력 실패면
+		while ((ch = getchar()) != '\n' && ch != EOF) { }
+		puts("숫자 입력 요망");
+	}
+
+	while ((ch = getchar()) != '\n' && ch != EOF) { }
+
+	if (index<1 || index>count) {
+		puts("입력범위 초과");
+		return;
+	}
+
+	puts("삭제할 정보 : ");
+	print_contact(index - 1);
+	// 여기까지 1차 작성
+
+	//2차 작성 시작
+	printf("정말 삭제하시겠습니까?> (y/n) > ");
+	read_line(yn, (int)sizeof(yn));
+
+	if (!yn[0]=='y' || !yn[0]=='Y') {	// 소문자 y이거나 대문자 Y가 아니면
+		puts("삭제를 취소합니다.");
+		return;
+	}
+
+	// 현재 삭제할 값 이후의 배열 요소를 앞으로 당김
+	for ( i = index-1; i < count; i++) {
+		contacts[i] = contacts[i + 1];	// 전부 앞으로..
+	}
+	count--;	// 한건 지웠으니 전체 수를 1 감소시킴
+
+	puts("삭제 완료!");
+
+}
+
+// 메뉴 6. 정렬 함수(퀵소트)
+static void sort_by_name() {
+	if (count<=1) {
+		puts("정렬 불필요");
+		return;
+	}
+
+	// 퀵소트
+	qsort(contacts, count, sizeof(Contact), cmp_contact_name);
+	puts("이름순 정렬 완료");
+}
+
+// 메뉴 8. 저장 함수
+
+static int save_contacts(const char* filename) {
+	FILE* fp = fopen(filename, "w");	// 쓰기 모드로 오픈
+	int i;
+
+	if (!fp) {	// fp == NULL 이면
+		puts("파일 저장 실패!(경로나 권한 확인요망)");
+		return 0;
+	}
+
+	// 저장 작업
+	for ( i = 0; i < count; i++) {
+		// 구분자 | 사용. |(파이프)를 주소록에 사용하면 문제발생!
+		fprintf(fp, "%s|%s|%s|%s|%s\n", 
+			contacts[i].name, 
+			contacts[i].phone, 
+			contacts[i].address, 
+			contacts[i].email, 
+			contacts[i].memo
+		);	// 한줄 지우기 shift + del
+	}
+
+
+	fclose(fp);
+	puts("저장 완료!");
+	return 1;	// 한건 성공
+}
+
+// 메뉴 9. 로드함수
+static int load_contacts(const char* filename) {
+	FILE* fp = fopen(filename, "r");
+	char line[600];
+
+	if (!fp) {
+		puts("불러올 파일이 없습니다.");
+		return 0;
+	}
+
+	count = 0;
+	while (fgets(line, sizeof(line), fp) != NULL) {	
+		char* name;
+		char* phone;
+		char* address;
+		char* email;
+		char* memo;
+
+		trim_newline(line);	// 메모 이후 \n을 삭제
+		
+		// |로 분리, 토큰 분리
+		name = strtok(line,"|");
+		phone = strtok(NULL, "|");
+		address = strtok(NULL, "|");
+		email = strtok(NULL, "|");
+		memo = strtok(NULL, "|");
+
+		// contacts 배엘에 하나씩 대입
+		// 다섯개 데이터중 하나라도 없으면 패스
+		if (!name || !phone || !email || !memo || !address) continue;
+
+		if (count >= MAX_CONTACTS) break;	// 현재 100개 밖에 못넣음
+
+		strncpy(contacts[count].name, name, NAME_LEN);
+		contacts[count].name[NAME_LEN - 1] = '\0';	// 이름 길이보다 길게 쓰면 정리
+
+		strncpy(contacts[count].phone, phone, PHONE_LEN);
+		contacts[count].phone[PHONE_LEN - 1] = '\0';	// 이름 길이보다 길게 쓰면 정리
+
+		strncpy(contacts[count].address, address, ADDR_LEN);
+		contacts[count].address[ADDR_LEN - 1] = '\0';	// 이름 길이보다 길게 쓰면 정리
+
+		strncpy(contacts[count].email, email, EMAIL_LEN);
+		contacts[count].email[EMAIL_LEN - 1] = '\0';	// 이름 길이보다 길게 쓰면 정리
+
+		strncpy(contacts[count].memo, memo, MEMO_LEN);
+		contacts[count].memo[MEMO_LEN - 1] = '\0';	// 이름 길이보다 길게 쓰면 정리
+
+		count++;
+	}
+
+	fclose(fp);
+	puts("불러오기 완료");
+}
+
+#pragma endregion 
---
+++ Day11/ToyProject01/Addressbook_step8/main.c
@@ -0,0 +1,86 @@
+﻿// 주소록 프로그램 step 8
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdlib.h>
+#include<stdio.h>
+
+#include "addressbook.h"
+
+
+#pragma region 메인함수 영역
+
+static void clear_screen() {
+	system("cls");	// 화면 클리어
+}
+
+static void pause_enter() {
+	puts("\n계속하려면 Enter...");
+	getchar();
+}
+
+int main() {
+	int choice = 0;
+
+	if (!ab_init()) {
+		puts("메모리 할당에 실패하여 프로그램을 종료합니다.");
+		return -1;
+	}
+
+	load_contacts("contacts.txt");	// 프로그램 실행 후 데이터 로드!
+
+	while (1) {	// 무한루프
+		clear_screen();
+		print_menu();	// 메뉴를 출력
+		choice = read_menu();	// choice 값에 입력을 받음
+
+		switch (choice) {
+		case 1:
+			//printf("%s\n", "[ADD] 연락처 추가 (기능 구현요)");
+			add_contact();		// 실제 연락처 추가
+			break;
+
+		case 2:
+			//puts("[LIST] 연락처 목록");
+			list_contact();
+			break;
+
+		case 3:
+			//puts("[SEARCH] 연락처 검색");
+			search_contact();
+			break;
+
+		case 4:
+			//puts("[EDIT] 연락처 수정");
+			update_contact();
+			break;
+
+		case 5:
+			//puts("[DELETE] 연락처 삭제");
+			delete_contact();
+			break;
+
+		case 6:
+			//puts("정렬");
+			sort_by_name();
+			break;
+
+		case 7:
+			save_contacts("contacts.txt");	// 종료 직전 현재 데이터 저장
+			ab_free();	// 반드시 메모리 해제
+			puts("프로그램 종료");
+			return 0;
+
+		default:
+			puts("메뉴는 1~7 사이 입니다.");
+			break;
+		}
+
+		pause_enter();
+
+	}
+
+	return 0;
+
+}
+#pragma endregion
+
